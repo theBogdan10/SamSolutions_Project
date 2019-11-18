9e54c9e8c05445f1df8199172cba761b5b19c028
Bogdan <bybogdan@mail.ru>
2019-11-18 15:22:45 +0300

    feat:add header and new info about weather

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-let appId='d4424ee2624d55b25cb2db519fe2123d';
-let units='metric';
+const appId='d4424ee2624d55b25cb2db519fe2123d';
+const units='metric';
 let searchMethod;
 
 
@@ -13,17 +12,28 @@ function getSearchMethod(searchTerm){
 
 function searchWeather(searchTerm){
     getSearchMethod(searchTerm)
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result =>{
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result =>{
         return result.json();
     }).then(result =>{
-        init(result)
-    })
+        //let arr=result.list.map(a=>a.dt_txt)
+        console.log(result.list)
+        // let new_arr=Object.values(arr)
+        // console.log(new_arr.lenght)
+        result.list.forEach(element => {
+            
+        });
+        //console.log(new_arr)   
+            load(result)
+            initBackground(result) 
+            
+    })//.catch(err=>console.error("Ooops! Try again"))
 
 }
 
 
-function init(resultFromServer){
-   switch (resultFromServer.weather[0].main) {
+
+function initBackground(resultFromServer){
+   switch (resultFromServer.list[0].weather[0].main) {
        case 'Clear':
             document.body.style.backgroundImage="url('img/clear.jpg')"
            break;
@@ -48,38 +58,40 @@ function init(resultFromServer){
 
            break;
    }
+}
 
-   let weatherDescriptionHeader=document.getElementById("weatherDescriptionHeader");
-   let temperatureElement=document.getElementById("temperature")
-   let humidityElement=document.getElementById("humidity")
-   let windSpeedElement=document.getElementById("windSpeed")
-   let cityHeader=document.getElementById("cityHeader")
-   let weatherIcon=document.getElementById("documentIconImg")
 
-   weatherIcon.src='http://openweathermap.org/img/wn/'+resultFromServer.weather[0].icon+'.png';
+   const weatherDescriptionHeader=document.getElementById("weatherDescriptionHeader");
+   const temperatureElement=document.getElementById("temperature")
+   const humidityElement=document.getElementById("humidity")
+   const windSpeedElement=document.getElementById("windSpeed")
+   const cityHeader=document.getElementById("cityHeader")
+   const weatherIcon=document.getElementById("documentIconImg")
+   const dateElement=document.getElementById("date")
+   const dayOfWeek=document.getElementById("dayHeader")
 
-   let resultDescription=resultFromServer.weather[0].description;
+
+function load(resultFromServer){
+   
+
+   weatherIcon.src='http://openweathermap.org/img/wn/'+resultFromServer.list[0].weather[0].icon+'.png';
+
+   let resultDescription=resultFromServer.list[0].weather[0].description;
 
    weatherDescriptionHeader.innerText=resultDescription.charAt(0).toUpperCase()+resultDescription.slice(1);
-   temperatureElement.innerHTML=Math.floor(resultFromServer.main.temp)+" &#8451";
-   windSpeedElement.innerHTML="Winds at  "+ Math.floor(resultFromServer.wind.speed)+"m/s";
-   cityHeader.innerHTML=resultFromServer.name;
-   humidityElement.innerHTML="Humidity levels at  "+resultFromServer.main.humidity+"%"
-
-   setPositionForWeatherInfo()
+   temperatureElement.innerHTML=Math.floor(resultFromServer.list[0].main.temp)+" &#8451";
+   windSpeedElement.innerHTML="Winds at   "+ Math.floor(resultFromServer.list[0].wind.speed)+"m/s";
+   cityHeader.innerHTML=resultFromServer.city.name;
+   humidityElement.innerHTML="Humidity levels at   "+resultFromServer.list[0].main.humidity+"%"
+   dateElement.innerHTML="Date: "+resultFromServer.list[0].dt_txt
+   dayOfWeek.innerHTML=getDay();
+   
+   let weatherContainer=document.getElementById("weatherContainer")
+   weatherContainer.style.visibility="visible";
 
 }
 
 
-
-function setPositionForWeatherInfo(){
-    let weatherContainer=document.getElementById("weatherContainer")
-    let weatherContainerHeight=weatherContainer.clientHeight;
-    let weatherContainerWidth=weatherContainer.clientWidth;
-
-    
-    weatherContainer.style.visibility="visible";
-}
  
 
 document.getElementById("searchBtn").addEventListener("click",()=>{
@@ -89,20 +101,32 @@ document.getElementById("searchBtn").addEventListener("click",()=>{
     }
 })
 
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    var btn = document.querySelector('.button'),
-        loader = document.querySelector('.loader'),
-        check = document.querySelector('.check');
-    
-    btn.addEventListener('click', function () {
-      loader.classList.add('active');    
-    });
-   
-    loader.addEventListener('animationend', function() {
-      check.classList.add('active'); 
-    });
-  });
-  
+function getDay(){
+    let day=new Date().getDay();
+    switch (day){
+        case 0:
+            day="Sunday"
+            break;
+        case 1:
+            day="Monday"
+            break;
+        case 2:
+            day="Tuesday"
+            break;
+        case 3:
+           day="Wednesday"
+           break;
+        case 4:
+           day="Thursday"
+           break;
+        case 5:
+           day="Friday"
+           break;
+        case 6:
+           day="Saturday"
+           break;                 
+       default:
+           break;
+   }
+   return day;
+}
